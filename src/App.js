a23aae78e43468092034cab039f448075b32d6fe
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-02-24 19:33:03 -0500

    Changed penguin first names into an object and changed code on line 57 to put a space in between the names

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  // const [ nameInput, setNameInput ] = useState('');
+  const [ nameInput, setNameInput ] = useState('');
 
-  // const handleOnChange = (event, index) => {
-  //   const newName = [...nameInput];
-  //   newName[index] = event.target.value;
-  //   setNameInput(newName);
-  // }
+  const handleOnChange = (event) => {
+    let newName = [...nameInput];
+    newName = event.target.value;
+    setNameInput(newName);
+  }
 
   // let penguinFirstNames =
-  // [Pecky, Chubbers, Beaky, Fishbreath, Fuzzbutt, Bigbeak, Pebbles, Fishcakes, Tumtums, Shrimpy, Featherface, Honkers, Blubbers, Squawky, Waddles, Puddles, Squidhead, Flippers, Fishface, Squeaks, Hoppy, Wobbles, Flappity, Chinstrap, Feathers, Stubby];
+  //   {
+  //     a: Pecky,
+  //     b: Chubbers,
+  //     c: Beaky,
+  //     d: Fishbreath,
+  //     e: Fuzzbutt,
+  //     f: Bigbeak,
+  //     g: Pebbles,
+  //     h: Fishcakes,
+  //     i: Tumtums,
+  //     j: Shrimpy,
+  //     k: Featherface, 
+  //     l: Honkers,
+  //     m: Blubbers,
+  //     n: Squawky,
+  //     o: Waddles,
+  //     p: Puddles,
+  //     q: Squidhead, 
+  //     r: Flippers,
+  //     s: Fishface,
+  //     t: Squeaks,
+  //     u: Hoppy,
+  //     v: Wobbles,
+  //     w: Flappity,
+  //     x: Chinstrap,
+  //     y: Feathers,
+  //     z: Stubby
+  //   };
+    
+    // console.log(penguinFirstNames.a);
 
   // let penguinMiddleNames = [Mc, Von];
+  
+  // letters A-M = Mc
+  // N-Z = Von
 
   // let penguinLastNames =
   // [Flapperson, Waddlebottom, Flapdoodle, Hoppington, Beakerson, Squeakybeak, Krillington, Featherbottom, Beady eyes, Roly poly, Bellyflop, Peckington, Pecks-a-lot, Squidbreath, Squaksalot, Fancyflippers, Hopsalot, Flappyfeet, Blubberbuns, Pebblepincher, Wigglefeet, Flapperson, Snowballs, Swimmington, Flipperstein, 'Slip\'n\'Slide'];
@@ -21,18 +53,23 @@ function App() {
   // let alphabet =
   // [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 
+  const grabCharacters = () => {
+    // let firstChars = console.log(nameInput.match(/[A-Z]/g).join(' '))
+  }
+
   return (
     <div className="app">
       <div className="name-question">
         What's your first & last name?
       </div>
       <input
-        // onChange={(event) => { return handleOnChange(event)}}
+        onChange={(event) => { return handleOnChange(event)}}
         placeholder="Type your name"
+        value={nameInput}
         className="name-input"
       />
-      <div className="submit-name-button">
-        Click & see your penguin name
+      <div onClick={grabCharacters} className="submit-name-button">
+        Click & see your penguin name 🐧
       </div>
       {/* <div className="resulting-penguin-name">
         Your penguin name 🐧:
